1616eb4315c89b280ea44c52d3399bcfa845d246
Noah <noah@gus.ucsb.edu>
2016-01-06 15:50:30 -0800

    home page lists projects

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from budget.models import Account, Fund, Sub, Project
 
-# Create your views here.
+def index(request):
+  projects = Project.objects.all()
+  context = { 'projects': projects }
+  return render(request, 'home.html', context)
+
