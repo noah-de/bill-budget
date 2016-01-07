0ddcb5520a24ed6648263a4ac6ab5ba4ccd98fab
Noah <noah@gus.ucsb.edu>
2016-01-07 15:58:46 -0800

    looping correctly

--- read_DW.py
+++ read_DW.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 
+#file=open('Short_test.xls')
 file=open('NRI_lafs_lookup.xls')
+accounts = []
 
 soup=BeautifulSoup(file, 'html.parser')
 table = soup.find('table')
@@ -13,13 +16,25 @@ for tr in rows:
  
   string = text.replace(u'\xa0', u'')
   elements = string.split(':::')
-# print(elements[1])
-  location = elements[1]
-  account = elements[2]
-  fund = elements[3]
-  sub = elements[4]
-  begin_date = elements[5]
-  end_date = elements[6]
-  print(elements[1:6])
+  
+  location = elements[0]
+  account = elements[1]
+  fund = elements[2]
+  sub = elements[3]
+  begin = elements[4]
+  end = elements[5]
+
+  if account not in accounts:
+    print('making new account')
+    accounts.append(account)
+    funds = []
+
+  if fund not in funds:
+    print('making new fund')
+    funds.append(fund)
+
+  end_date = (datetime(int(end[:4]), int(end[4:6]), int(end[-2:])))
+  if(end_date > datetime.now()):
+    print(elements[:6])
 
 # Loc,Account,Fund,Sub,Lafs Begin Date,Lafs End Date,Lafs Open Trans Flag,Fin Department Code,Cp4 Code,Cp3 Code,Cp2 Code,Cp1 Code,Account Title,Fund Title,Laf Title,Prin Invest Last Name,Sponsor Desc,Fiscal Year,Overhead base code,Overhead base desc,Overhead rate
