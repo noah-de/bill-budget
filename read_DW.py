fd79488ce27d0de747b3a3ef153591ed0c8025a2
Noah <noah@gus.ucsb.edu>
2016-01-07 14:07:57 -0800

    better parsing

--- read_DW.py
+++ read_DW.py
@@ -13,4 +13,13 @@ for tr in rows:
  
   string = text.replace(u'\xa0', u'')
   elements = string.split(':::')
-  print(elements[1])
+# print(elements[1])
+  location = elements[1]
+  account = elements[2]
+  fund = elements[3]
+  sub = elements[4]
+  begin_date = elements[5]
+  end_date = elements[6]
+  print(elements[1:6])
+
+# Loc,Account,Fund,Sub,Lafs Begin Date,Lafs End Date,Lafs Open Trans Flag,Fin Department Code,Cp4 Code,Cp3 Code,Cp2 Code,Cp1 Code,Account Title,Fund Title,Laf Title,Prin Invest Last Name,Sponsor Desc,Fiscal Year,Overhead base code,Overhead base desc,Overhead rate
